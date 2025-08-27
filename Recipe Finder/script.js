9eeba4e1c8b83518bd47029157c3de348ad5f268
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-08-27 16:57:10 +0300

    Update search button styles and enable/disable functionality based on input; enhance user interaction and feedback

--- Recipe Finder/script.js
+++ Recipe Finder/script.js
@@ -28,131 +28,141 @@ const searchbtn = document.getElementById('search-btn');
 let temptext = document.querySelector('.temporarytext');
 const categoryDisplayContainer = document.getElementById('categoryDisplayContainer');
 
-store.data = {
-    activeFilterButton: 'name',
-    searchText: '',
-    Visible: false,
-    selectedCategory: 'All Categories'
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-store.subscribe(newState => {
-    categoryDisplayContainer.innerHTML = "";
+    store.data = {
+        activeFilterButton: 'name',
+        searchText: '',
+        Visible: false,
+        selectedCategory: 'All Categories'
+    }
 
-    if (newState.selectedCategory === 'All Categories') return;
+    store.subscribe(newState => {
+        categoryDisplayContainer.innerHTML = "";
 
-    const categoryDisplay = document.createElement('div')
-    categoryDisplay.classList.add('category-display');
+        if (newState.selectedCategory === 'All Categories') return;
 
-    const categoryResult = document.createElement('p');
-    categoryResult.classList.add('category-result');
-    categoryResult.textContent = 'Category:';
+        const categoryDisplay = document.createElement('div')
+        categoryDisplay.classList.add('category-display');
 
-    const categoriesSpan = document.createElement('span');
-    categoriesSpan.classList.add('categories');
-    categoriesSpan.textContent = newState.selectedCategory;
+        const categoryResult = document.createElement('p');
+        categoryResult.classList.add('category-result');
+        categoryResult.textContent = 'Category:';
 
-    const clearbtn = document.createElement('button');
-    clearbtn.type = 'button';
-    clearbtn.textContent = 'clear';
-    clearbtn.classList.add('clearbtn');
-    clearbtn.addEventListener('click', () => {
-        store.update({ selectedCategory: 'All Categories' });
-    });
+        const categoriesSpan = document.createElement('span');
+        categoriesSpan.classList.add('categories');
+        categoriesSpan.textContent = newState.selectedCategory;
 
-    categoryDisplay.append(categoryResult, categoriesSpan, clearbtn);
-    categoryDisplayContainer.appendChild(categoryDisplay);
-    categoryPart.appendChild(categoryDisplayContainer);
-
-});
-
-function populateDropdown() {
-    dropdownList.innerHTML = '';
-
-    Array.from(hiddenSelect.options).forEach(option => {
-        const dropdownItem = document.createElement('div');
-        dropdownItem.className = 'custom-dropdown-item';
-        dropdownItem.textContent = option.textContent;
-        dropdownItem.setAttribute('data-value', option.value);
-
-        dropdownItem.addEventListener('click', (event) => {
-            event.stopPropagation();
-            const selectedText = event.target.textContent;
-            const selectedValue = event.target.getAttribute('data-value');
-            hiddenSelect.value = selectedValue;
-            store.update({ selectedCategory: selectedText, Visible: false });
+        const clearbtn = document.createElement('button');
+        clearbtn.type = 'button';
+        clearbtn.textContent = 'clear';
+        clearbtn.classList.add('clearbtn');
+        clearbtn.addEventListener('click', () => {
+            store.update({ selectedCategory: 'All Categories' });
         });
-        dropdownList.appendChild(dropdownItem);
+
+        categoryDisplay.append(categoryResult, categoriesSpan, clearbtn);
+        categoryDisplayContainer.appendChild(categoryDisplay);
+        categoryPart.appendChild(categoryDisplayContainer);
 
     });
-}
 
-if (!temptext) {
-    temptext = document.createElement('div');
-    temptext.classList.add('temporarytext');
-    const text1 = document.createElement('span');
-    text1.classList.add('text1');
-    text1.textContent = "🔍";
-    const text2 = document.createElement('span');
-    text2.classList.add('text2');
-    text2.textContent = "No recipes found";
-    const text3 = document.createElement('span');
-    text3.classList.add('text3');
-    text3.textContent = "Try searching with different keywords or ingredients";
-    temptext.append(text1, text2, text3);
-    container.append(temptext);
-}
-if (recipeCount > 0) {
-    console.log("Kart var, işlemleri başlat!");
-    temptext.style.display = 'none';
-} else {
-    console.log("Kart yok!");
-}
+    function populateDropdown() {
+        dropdownList.innerHTML = '';
 
-store.subscribe(newState => {
-    searchInput.placeholder = `Search by ${newState.activeFilterButton}...`;
+        Array.from(hiddenSelect.options).forEach(option => {
+            const dropdownItem = document.createElement('div');
+            dropdownItem.className = 'custom-dropdown-item';
+            dropdownItem.textContent = option.textContent;
+            dropdownItem.setAttribute('data-value', option.value);
 
-    const activeButton = newState.activeFilterButton === 'name' ? nameButton : ingredientButton;
-    const inActiveButton = newState.activeFilterButton === 'name' ? ingredientButton : nameButton;
+            dropdownItem.addEventListener('click', (event) => {
+                event.stopPropagation();
+                const selectedText = event.target.textContent;
+                const selectedValue = event.target.getAttribute('data-value');
+                hiddenSelect.value = selectedValue;
+                store.update({ selectedCategory: selectedText, Visible: false });
+            });
+            dropdownList.appendChild(dropdownItem);
 
-    activeButton.style.cssText = css`
+        });
+    }
+
+    if (!temptext) {
+        temptext = document.createElement('div');
+        temptext.classList.add('temporarytext');
+        const text1 = document.createElement('span');
+        text1.classList.add('text1');
+        text1.textContent = "🔍";
+        const text2 = document.createElement('span');
+        text2.classList.add('text2');
+        text2.textContent = "No recipes found";
+        const text3 = document.createElement('span');
+        text3.classList.add('text3');
+        text3.textContent = "Try searching with different keywords or ingredients";
+        temptext.append(text1, text2, text3);
+        container.append(temptext);
+    }
+    if (recipeCount > 0) {
+        console.log("Kart var, işlemleri başlat!");
+        temptext.style.display = 'none';
+    } else {
+        console.log("Kart yok!");
+    }
+
+    store.subscribe(newState => {
+        searchInput.placeholder = `Search by ${newState.activeFilterButton}...`;
+
+        const activeButton = newState.activeFilterButton === 'name' ? nameButton : ingredientButton;
+        const inActiveButton = newState.activeFilterButton === 'name' ? ingredientButton : nameButton;
+
+        activeButton.style.cssText = css`
             background-color: rgb(38, 116, 233);
             color: white;
         `
 
-    inActiveButton.style.cssText = css`
+        inActiveButton.style.cssText = css`
             color: rgb(51, 51, 51);
             background-color: rgb(240, 238, 238);
         `
-})
+    })
 
-nameButton.addEventListener('click', () => store.update({ activeFilterButton: 'name' }));
-ingredientButton.addEventListener('click', () => store.update({ activeFilterButton: 'ingredient' }));
+    nameButton.addEventListener('click', () => store.update({ activeFilterButton: 'name' }));
+    ingredientButton.addEventListener('click', () => store.update({ activeFilterButton: 'ingredient' }));
 
-store.subscribe(newState => {
-    const searchText = newState.searchText;
-    if (searchText.length > 0) {
-        searchbtn.style.cursor = 'pointer';
-        searchbtn.style.backgroundColor = 'rgb(38, 116, 233)';
-    }
-    else {
-        searchbtn.style.cursor = 'no-drop';
-        searchbtn.style.backgroundColor = 'rgba(38, 116, 233, 0.4)';
-    }
-})
+    store.subscribe(newState => {
+        const searchText = newState.searchText;
+        if (searchText.length > 0) {
+            searchbtn.disabled = false;
+            searchbtn.style.cursor = 'pointer';
+            searchbtn.style.backgroundColor = 'rgb(38, 116, 233)';
+        }
+        else {
+            searchbtn.disabled = true;
+            searchbtn.style.cursor = 'no-drop';
+            searchbtn.style.backgroundColor = 'rgba(38, 116, 233, 0.4)';
+        }
+    })
 
-searchInput.addEventListener("input", () => store.update({ searchText: searchInput.value.trim() }));
+    searchInput.addEventListener("input", () => store.update({ searchText: searchInput.value.trim() }));
 
-populateDropdown();
+    searchbtn.addEventListener('click', () => {
+        console.log("buton aktif");
+    });
 
-store.subscribe(newState => {
-    dropdownList.style.display = newState.Visible ? 'block' : 'none';
-})
-categorySelect.addEventListener('click', () => store.update({ Visible: !store.data.Visible }));
+    populateDropdown();
 
-document.addEventListener('click', (event) => {
-    if (!categorySelect.contains(event.target) && !dropdownList.contains(event.target)) {
-        store.update({ Visible: false })
-    }
+    store.subscribe(newState => {
+        dropdownList.style.display = newState.Visible ? 'block' : 'none';
+    })
+    categorySelect.addEventListener('click', () => store.update({ Visible: !store.data.Visible }));
+
+    document.addEventListener('click', (event) => {
+        if (!categorySelect.contains(event.target) && !dropdownList.contains(event.target)) {
+            store.update({ Visible: false })
+        }
+    });
+
 });
 
 
