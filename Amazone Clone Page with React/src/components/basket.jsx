7f56104cd83979c2e8991397bbdd49d78ba757e2
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-18 14:53:45 +0300

    Add informative text to empty cart message in basket component. Enhanced user experience by providing guidance on shopping options and encouraging product selection.

--- Amazone Clone Page with React/src/components/basket.jsx
+++ Amazone Clone Page with React/src/components/basket.jsx
@@ -16,7 +16,7 @@ const Cart = ({ setCartCount }) => {
       })
       .catch((err) => console.log("Clear cart error", err));
   };
-  
+
   useEffect(() => {
     fetch("http://localhost:3001/api/basket")
       .then((res) => res.json())
@@ -32,13 +32,23 @@ const Cart = ({ setCartCount }) => {
       <div className="product-main">
         <div className="Carttext-Part">
           <h2>Amazon sepetiniz bos</h2>
+          <p>
+            Alışveriş Sepetiniz size hizmet etmek için var. Market ürünleri,
+            giyim, ev malzemeleri, elektronik ürünler ve daha fazlası ile
+            doldurarak ona bir amaç verin.
+          </p>
+          <p>
+            Amazon.com.tr sayfasında alışverişe devam edin, günün fırsatları
+            hakkında bilgi alın veya İstek Listesi bağlantısını kullanarak
+            listelerinize göz atın.
+          </p>
         </div>
       </div>
     );
   }
   return (
     <div className="product-main">
-      <ClearToCartButton onClearToCart={handleClearCart}/>;
+      <ClearToCartButton onClearToCart={handleClearCart} />;
       {basketItems.map((item, index) => (
         <ProductCard
           key={item.id || `item-${index}`}
