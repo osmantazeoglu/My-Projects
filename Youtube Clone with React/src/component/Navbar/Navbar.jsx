1b1cdfda5c23ba75baa5547fea97c10e8b93d051
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-27 01:32:28 +0300

    Add Navbar CSS styles for flexbox layout

--- Youtube Clone with React/src/component/Navbar/Navbar.jsx
+++ Youtube Clone with React/src/component/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/Navbar.css"
 import NavbarLeft from "./Navbar-Left";
 import NavbarCenter from "./Navbar-Center";
 import NavbarRight from "./Navbar-Right";
