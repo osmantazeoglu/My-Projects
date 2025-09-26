7c3787885e226aa0a9df934464fa8dd6c97fcbf2
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-27 00:47:44 +0300

    Remove unused react.svg asset from project to streamline asset management and reduce file size.

--- Youtube Clone with React/videos.js
+++ Youtube Clone with React/videos.js
@@ -6,6 +6,15 @@ export const videos = [
     Description: "Rick Astley - Never Gonna Give You Up",
     Channel: "Rick Astley",
     Views: 1697297269,
-    publicationDate: 6
+    publicationDate: "15 years ago"
+},
+{
+    id: 2,
+    video: "https://www.youtube.com/embed/7nT7JGZMbtM",
+    logo: "https://yt3.googleusercontent.com/eIf5fNPcIcj9ig-wZBeq4stFy1lgjWTW1nLT5dYlFkHZprZ03QBiMcbpwNMB6XSBjrSFGtAGQg=s160-c-k-c0x00ffffff-no-rj",
+    Description: "James Webb Space Telescope Launch — Official NASA Broadcast",
+    Channel: "NASA",
+    Views: 11000000,
+    publicationDate: "8 days ago" 
 }
 ];
