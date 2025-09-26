ca9ea96eec40131ddbcb708f46cf683397b3e147
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-26 21:13:38 +0300

    Refactor NavbarRight component to enhance layout with new buttons for creating content, notifications, and user avatar. Update CSS for improved styling and hover effects.

--- Youtube Clone with React/src/component/Navbar-Right.jsx
+++ Youtube Clone with React/src/component/Navbar-Right.jsx
@@ -1,9 +1,17 @@
-function NavbarRight(){
-    return(
-        <div>
-            <p>create</p>
-        </div>
-    );
+import { FaBell } from "react-icons/fa";
+function NavbarRight() {
+  return (
+    <nav className="navbar-right">
+      <button className="create-btn">
+        <span className="plus-icon">+</span>
+        Create
+      </button>
+      <button className="Notification-btn">
+        <FaBell />
+      </button>
+      <button className="avatar-btn">O</button>
+    </nav>
+  );
 }
 
 export default NavbarRight;
