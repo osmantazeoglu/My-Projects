04cf841d3bf61f50abdc68a25a9b56c138eefe86
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-27 02:18:53 +0300

    Refactor NavbarCenter component and update CSS styles for search box and icons to enhance layout and usability

--- Youtube Clone with React/src/component/Navbar/Navbar-Center.jsx
+++ Youtube Clone with React/src/component/Navbar/Navbar-Center.jsx
@@ -3,13 +3,15 @@ import { FaMicrophone, FaSearch } from "react-icons/fa";
 function NavbarCenter() {
   return (
     <nav className="navbar-center flex-div">
-      <input className="Search-part" type="text" placeholder="Search" />
-      <button className="Search-icon">
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+      </div>
+      <button className="search-icon">
         <FaSearch />
       </button>
 
       <button className="mic-icon">
-        <FaMicrophone/>
+        <FaMicrophone />
       </button>
     </nav>
   );
