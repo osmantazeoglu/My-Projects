1463b7fcec6d38d94ee12aa1e59e229f423dec70
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-27 00:18:24 +0300

    Add video player to App component and improve layout structure. Update JSX formatting for consistency and readability.

---
+++ Youtube Clone with React/videos.js
@@ -0,0 +1,11 @@
+export const videos = [
+{
+    id: 1,
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    logo: "https://yt3.googleusercontent.com/vewxKdtll-rntHAMDPY_Qa6hPac3-J2sCFFUwvGSSR0i7hB4g5rNfF39lCEcjGvsYI0RWx7V1A=s160-c-k-c0x00ffffff-no-rj",
+    Description: "Rick Astley - Never Gonna Give You Up",
+    Channel: "Rick Astley",
+    Views: 1697297269,
+    publicationDate: 6
+}
+];
