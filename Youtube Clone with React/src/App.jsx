a8f2d1d8a6857c2c30b948afb351fbab05befe2f
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-29 16:47:39 +0300

    Add routing to App component with Home and Video routes for improved navigation

--- Youtube Clone with React/src/App.jsx
+++ Youtube Clone with React/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
+import { Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
     </div>
   );
 }
