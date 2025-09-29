a39b536ed3cda7370683da57c6fa7798498c69c8
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-29 20:54:15 +0300

    Enhance Subscriptions component in Sidebar with new channel images and titles. Update Sidebar.css for improved layout, hover effects, and styling adjustments.

--- Youtube Clone with React/src/component/Sidebar/Subscriptions.jsx
+++ Youtube Clone with React/src/component/Sidebar/Subscriptions.jsx
@@ -1,12 +1,37 @@
-import home from "../../assets/Youtube-Home.png";
+import jack from "../../assets/jack.png"
+import simon from "../../assets/simon.png"
+import tom from "../../assets/tom.png"
+import megan from "../../assets/megan.png"
+import cameron from "../../assets/cameron.png"
+import gerard from "../../assets/gerard.png"
 
 function Subscriptions() {
   return (
     <div className="subscriptions">
       <h4>Subscriptions</h4>
       <div className="sub-list side-list">
-        <img src={home} alt="" className="side-img" />
-        <p className="side-text">Home</p>
+        <img src={jack} alt="" className="sub-img" />
+        <p className="side-text">TechVision</p>
+      </div>
+      <div className="sub-list side-list">
+        <img src={simon} alt="" className="sub-img" />
+        <p className="side-text">GamerZone</p>
+      </div>
+      <div className="sub-list side-list">
+        <img src={tom} alt="" className="sub-img" />
+        <p className="side-text">TravelTales</p>
+      </div>
+      <div className="sub-list side-list">
+        <img src={megan} alt="" className="sub-img" />
+        <p className="side-text">MusicHub</p>
+      </div>
+      <div className="sub-list side-list">
+        <img src={cameron} alt="" className="sub-img" />
+        <p className="side-text">DailyDose</p>
+      </div>
+      <div className="sub-list side-list">
+        <img src={gerard} alt="" className="sub-img" />
+        <p className="side-text">FoodFactory</p>
       </div>
 
       <hr />
