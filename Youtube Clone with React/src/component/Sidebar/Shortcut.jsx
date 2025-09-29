fe0a0ecf60f0005b2c3b72c68113b111263475ba
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-29 17:47:13 +0300

    Refactor Sidebar component to utilize Shortcut for improved code organization and update styling in Sidebar.css for better layout and alignment.

---
+++ Youtube Clone with React/src/component/Sidebar/Shortcut.jsx
@@ -0,0 +1,31 @@
+import "../../styles/Sidebar.css";
+import shorts from "../../assets/youtube-shorts.svg";
+import home from "../../assets/Youtube-Home.png";
+import subscription from "../../assets/youtube-subscriptions.svg";
+import music from "../../assets/Youtube-Music.png";
+
+function Shortcut() {
+  return (
+    <div className="shortcut-link">
+      <div className="side-link">
+        <img src={home} alt="" />
+        <p>Home</p>
+      </div>
+      <div className="side-link">
+        <img src={shorts} alt="" />
+        <p>Shorts</p>
+      </div>
+      <div className="side-link">
+        <img src={subscription} alt="" />
+        <p>Subscription</p>
+      </div>
+      <div className="side-link">
+        <img src={music} alt="" />
+        <p>Youtube Music</p>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default Shortcut;
--- Youtube Clone with React/src/component/Sidebar/sidebar.jsx
+++ Youtube Clone with React/src/component/Sidebar/sidebar.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import "../../styles/Sidebar.css";
-import shorts from "../../assets/youtube-shorts.svg"
-
+import Shortcut from "./Shortcut";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sortcut-link">
-        <div className="side-link">
-          <p>Home</p>
-        </div>
-        <div className="side-link">
-            <img src={shorts} alt="" /><p>Shorts</p>
-        </div>
-        <div className="side-link"></div>
-        <div className="side-link"></div>
-        <hr />
-      </div>
+      <Shortcut />
       <div className="subscribe-list">
         <h3>Subscriptions</h3>
       </div>
