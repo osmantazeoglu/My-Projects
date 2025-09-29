e0a93a68deeabe9d913e59a8ceb1e07b007b3c6b
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-29 17:27:02 +0300

    Update favicon path in index.html and remove unused asset images to streamline project structure. Enhance Sidebar component with new layout and styling in Sidebar.css.

--- Youtube Clone with React/src/component/Sidebar/sidebar.jsx
+++ Youtube Clone with React/src/component/Sidebar/sidebar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import "../../styles/Sidebar.css"
+import "../../styles/Sidebar.css";
 
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="sortcut-link">
+        <div className="side-link">
+          <p>Home</p>
+        </div>
+        <div className="side-link"></div>
+        <div className="side-link"></div>
+        <div className="side-link"></div>
+        <hr />
+      </div>
+      <div className="subscribe-list">
+        <h3>Subscriptions</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
