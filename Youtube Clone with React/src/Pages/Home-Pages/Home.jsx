472a52c12e5125b8fecd353bdacf3980efeb6815
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-09-30 17:37:48 +0300

    Refactor Feed component to include thumbnail image and description, and update import path for consistency. Adjust HomePage layout for improved readability.

--- Youtube Clone with React/src/Pages/Home-Pages/Home.jsx
+++ Youtube Clone with React/src/Pages/Home-Pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../component/Sidebar/sidebar";
-import Feed from "../../component/Feed/feed";
+import Feed from "../../component/Feed/Feed";
 
 function HomePage({ sidebar }) {
   return (
     <>
       <Sidebar sidebar={sidebar} />
-      <div className={`container ${sidebar?"":"large-container"}`}>
-        <Feed/>        
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
       </div>
     </>
   );
--- Youtube Clone with React/src/component/Feed/Feed.jsx
+++ Youtube Clone with React/src/component/Feed/Feed.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../../styles/Feed.css";
+import thumbnail1 from "../../assets/thumbnail1.png";
 
-function Feed(){
-    return(
-        <div></div>
-    );
+function Feed() {
+  return (
+    <div className="card">
+      <img src={thumbnail1} alt="" />
+      <h2>Best channel to learn coding that help you to be a web developer</h2>
+    </div>
+  );
 }
 
 export default Feed;
