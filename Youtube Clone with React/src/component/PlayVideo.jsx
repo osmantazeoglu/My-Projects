d18b4381fafa820e2c5fd88dd13affe85765b35d
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-10-01 17:34:39 +0300

    Remove YouTube dislike image and update PlayVideo component to enhance video information display, including view count and like/dislike metrics.

--- Youtube Clone with React/src/component/PlayVideo.jsx
+++ Youtube Clone with React/src/component/PlayVideo.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import "../styles/PlayVideo.css";
 import video1 from "../assets/video.mp4";
-import share from "../assets/share.svg";
-import settings from "../assets/settings.svg";
-import save from "../assets/saved-icon.svg";
-import download from "../assets/youtube-download.svg";
 import like from "../assets/youtube-like.png";
-import dislike from "../assets/youtube-dislike.png";
+import dislike from "../assets/youtube-dislike.jpg"
 
 function PlayVideo() {
   return (
     <div className="play-video">
       <video src={video1} controls autoPlay muted></video>
-      <div className="video-controls">
-        <img src={share} alt="share" />
-        <img src={settings} alt="settings" />
-        <img src={save} alt="save" />
-        <img src={download} alt="download" />
-        <img src={like} alt="like" />
-        <img src={dislike} alt="dislike" />
+      <h3>Best Youtube Channel To Learn Web Developer</h3>
+      <div className="play-video-info">
+        <p>15k Views &bull; 2 days ago</p>
+        <div>
+          <span><img src={like} alt="" />4.8k</span>
+          <span><img src={dislike} alt="" />2k</span>
+        </div>
       </div>
     </div>
   );
