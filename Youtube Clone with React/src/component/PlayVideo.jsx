2e78f5df5319d645514fffe03248e04b1323c462
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-10-02 16:03:16 +0300

    Enhance PlayVideo component by adding share, save, and download features, along with a publisher section and multiple comments for improved user interaction and engagement.

--- Youtube Clone with React/src/component/PlayVideo.jsx
+++ Youtube Clone with React/src/component/PlayVideo.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/PlayVideo.css";
 import video1 from "../assets/video.mp4";
 import like from "../assets/youtube-like.png";
-import dislike from "../assets/youtube-dislike.jpg"
+import dislike from "../assets/youtube-dislike.jpg";
+import share from "../assets/share.svg";
+import save from "../assets/saved-icon.svg";
+import download from "../assets/youtube-download.svg";
+import jack from "../assets/jack.png";
 
 function PlayVideo() {
   return (
@@ -12,8 +16,129 @@ function PlayVideo() {
       <div className="play-video-info">
         <p>15k Views &bull; 2 days ago</p>
         <div>
-          <span><img src={like} alt="" />4.8k</span>
-          <span><img src={dislike} alt="" />2k</span>
+          <span>
+            <img src={like} alt="" />
+            4.8k
+          </span>
+          <span>
+            <img src={dislike} alt="" />
+            2k
+          </span>
+          <span>
+            <img src={share} alt="" />
+            Share
+          </span>
+          <span>
+            <img src={save} alt="" />
+            Save
+          </span>
+          <span>
+            <img src={download} alt="" />
+            Download
+          </span>
+        </div>
+      </div>
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>Greatstcak</p>
+          <p>2.5M Subscribers</p>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="video-description">
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe Greatstcak to watch more videos</p>
+        <h4>130 Comments</h4>
+        <div className="comment">
+          <img src={jack} alt="" />
+          <div>
+            <h3>
+              Jack Nelson <span>2 days ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" className="side-img" />
+              <span>244</span>
+              <img src={dislike} alt="" className="side-img" />
+              <span>175</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={jack} alt="" />
+          <div>
+            <h3>
+              Jack Nelson <span>2 days ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" className="side-img" />
+              <span>244</span>
+              <img src={dislike} alt="" className="side-img" />
+              <span>175</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={jack} alt="" />
+          <div>
+            <h3>
+              Jack Nelson <span>2 days ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" className="side-img" />
+              <span>244</span>
+              <img src={dislike} alt="" className="side-img" />
+              <span>175</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={jack} alt="" />
+          <div>
+            <h3>
+              Jack Nelson <span>2 days ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" className="side-img" />
+              <span>244</span>
+              <img src={dislike} alt="" className="side-img" />
+              <span>175</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={jack} alt="" />
+          <div>
+            <h3>
+              Jack Nelson <span>2 days ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" className="side-img" />
+              <span>244</span>
+              <img src={dislike} alt="" className="side-img" />
+              <span>175</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
