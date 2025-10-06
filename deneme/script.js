7c96dbe8e9f06c7fda19cc383174dd26059de798
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-10-06 15:14:39 +0300

    Add interactive elements to index.html and implement event handling in script.js, including event delegation for menu items and dynamic list updates.

--- deneme/script.js
+++ deneme/script.js
@@ -1,3 +1,4 @@
+// Closure
 function bankAccount(balance) {
   return {
     deposit: (amount) => {
@@ -20,3 +21,35 @@ console.log(myAccount.deposit(50));
 console.log(myAccount.withdraw(250));
 console.log(myAccount.deposit(50));
 console.log(myAccount.withdraw(150));
+
+// Event Bubbling and Event Delegation
+document.getElementById("parent").addEventListener("click", () => {
+  console.log("parent div tiklandi");
+});
+
+document.getElementById("child").addEventListener("click", () => {
+  console.log("child buton tiklandi");
+});
+
+document.getElementById("menu").addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    console.log(event.target.innerText + " tıklandı!");
+  }
+});
+
+document.getElementById("add").addEventListener("click", () => {
+  const newLi = document.createElement("li");
+  newLi.innerText = "Adress";
+  menu.appendChild(newLi);
+});
+
+function sayHello(name) {
+  return "Merhaba " + name;
+}
+
+const sayHello1 = (name1) => {
+  return "Merhaba " + name1;
+};
+
+console.log(sayHello1("Osman"));
+console.log(sayHello("Osman"));
