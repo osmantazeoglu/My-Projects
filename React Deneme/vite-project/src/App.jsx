21749f32804ddc3983c0d692da46138cc7241f9b
osmantazeoglu <osmanntazeoglu@gmail.com>
2025-10-20 01:38:29 +0300

    Refactor App component in App.jsx to include Hello and Message components for improved user interaction; enhance greeting and message display with styled text.

--- React Deneme/vite-project/src/App.jsx
+++ React Deneme/vite-project/src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
-
-function App() {
-  
-
+function Hello() {
   return (
     <>
+      <p>
+        <span style={{ color: "lightgreen" }}>Hello</span> Osman!
+      </p>
+      <p>Welcome to learning React</p>
     </>
-  )
+  );
 }
 
-export default App
+function Message({ user, message }) {
+  return (
+    <>
+      <p>
+        <span style={{ fontWeight: "bold", marginRight: "6px" }}>{user} </span>
+        <span>{message}</span>
+      </p>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Hello />
+      <Message user="Ali:" message="The weather is very nice today." />
+    </>
+  );
+}
+
+export default App;
